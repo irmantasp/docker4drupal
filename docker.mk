fbbef8d30fabc272d93e4ff55e4da4c720e03dc8
Irmantas Puodžiukas <irmantas.puodziukas@gmail.com>
2019-10-20 14:21:03 +0300

    Add irmantasp/drupal-project specific config

--- docker.mk
+++ docker.mk
@@ -5,7 +5,7 @@ default: up
 COMPOSER_ROOT ?= /var/www/html
 DRUPAL_ROOT ?= /var/www/html/web
 
-## help	:	Print commands help.
+## help	    :	Print commands help.
 .PHONY: help
 ifneq (,$(wildcard docker.mk))
 help : docker.mk
@@ -15,30 +15,30 @@ help : Makefile
 	@sed -n 's/^##//p' $<
 endif
 
-## up	:	Start up containers.
+## up	    :	Start up containers.
 .PHONY: up
 up:
 	@echo "Starting up containers for $(PROJECT_NAME)..."
 	docker-compose pull
 	docker-compose up -d --remove-orphans
 
-## down	:	Stop containers.
+## down	    :	Stop containers.
 .PHONY: down
 down: stop
 
-## start	:	Start containers without updating.
+## start	    :	Start containers without updating.
 .PHONY: start
 start:
 	@echo "Starting containers for $(PROJECT_NAME) from where you left off..."
 	@docker-compose start
 
-## stop	:	Stop containers.
+## stop	    :	Stop containers.
 .PHONY: stop
 stop:
 	@echo "Stopping containers for $(PROJECT_NAME)..."
 	@docker-compose stop
 
-## prune	:	Remove containers and their volumes.
+## prune	    :	Remove containers and their volumes.
 ##		You can optionally pass an argument with the service name to prune single container
 ##		prune mariadb	: Prune `mariadb` container and remove its volumes.
 ##		prune mariadb solr	: Prune `mariadb` and `solr` containers and remove their volumes.
@@ -47,37 +47,56 @@ prune:
 	@echo "Removing containers for $(PROJECT_NAME)..."
 	@docker-compose down -v $(filter-out $@,$(MAKECMDGOALS))
 
-## ps	:	List running containers.
+## ps	    :	List running containers.
 .PHONY: ps
 ps:
 	@docker ps --filter name='$(PROJECT_NAME)*'
 
-## shell	:	Access `php` container via shell.
+## shell	    :	Access `php` container via shell.
 .PHONY: shell
 shell:
 	docker exec -ti -e COLUMNS=$(shell tput cols) -e LINES=$(shell tput lines) $(shell docker ps --filter name='$(PROJECT_NAME)_php' --format "{{ .ID }}") sh
 
-## composer	:	Executes `composer` command in a specified `COMPOSER_ROOT` directory (default is `/var/www/html`).
+## composer   :	Executes `composer` command in a specified `COMPOSER_ROOT` directory (default is `/var/www/html`).
 ##		To use "--flag" arguments include them in quotation marks.
 ##		For example: make composer "update drupal/core --with-dependencies"
 .PHONY: composer
 composer:
 	docker exec $(shell docker ps --filter name='^/$(PROJECT_NAME)_php' --format "{{ .ID }}") composer --working-dir=$(COMPOSER_ROOT) $(filter-out $@,$(MAKECMDGOALS))
 
-## drush	:	Executes `drush` command in a specified `DRUPAL_ROOT` directory (default is `/var/www/html/web`).
+## drush	    :	Executes `drush` command in a specified `DRUPAL_ROOT` directory (default is `/var/www/html/web`).
 ##		To use "--flag" arguments include them in quotation marks.
 ##		For example: make drush "watchdog:show --type=cron"
 .PHONY: drush
 drush:
-	docker exec $(shell docker ps --filter name='^/$(PROJECT_NAME)_php' --format "{{ .ID }}") drush -r $(DRUPAL_ROOT) $(filter-out $@,$(MAKECMDGOALS))
+	docker exec $(shell docker ps --filter name='^/$(PROJECT_NAME)_php' --format "{{ .ID }}") vendor/bin/drush -r $(DRUPAL_ROOT) $(filter-out $@,$(MAKECMDGOALS))
 
-## logs	:	View containers logs.
+## drupal	    :	Executes `drupal` command in a specified `DRUPAL_ROOT` directory (default is `/var/www/html/web`).
+##		To use "--flag" arguments include them in quotation marks.
+##		For example: make drupal "list generate --format=txt"
+.PHONY: drupal
+drupal:
+	docker exec $(shell docker ps --filter name='^/$(PROJECT_NAME)_php' --format "{{ .ID }}") vendor/bin/drupal -root=$(DRUPAL_ROOT) $(filter-out $@,$(MAKECMDGOALS))
+
+
+## logs	    :	View containers logs.
 ##		You can optinally pass an argument with the service name to limit logs
 ##		logs php	: View `php` container logs.
 ##		logs nginx php	: View `nginx` and `php` containers logs.
 .PHONY: logs
 logs:
 	@docker-compose logs -f $(filter-out $@,$(MAKECMDGOALS))
+
+## sanitize :	Sanitize database tables.
+.PHONY: sanitize
+sanitize:
+	docker exec $(shell docker ps --filter name='^/$(PROJECT_NAME)_php' --format "{{ .ID }}") vendor/bin/drush -r $(DRUPAL_ROOT) sql-sanitize --sanitize-password=password --sanitize-email=%uid@$(PROJECT_BASE_URL)
+
+## backup :	Backup current database.
+.PHONY: backup
+backup:
+	docker exec $(shell docker ps --filter name='^/$(PROJECT_NAME)_php' --format "{{ .ID }}") vendor/bin/drush -r $(DRUPAL_ROOT) sql-dump > /var/www/html/docker/mariadb/dumps/$(shell date +'%Y%m%d-%H%i%S')--$(shell git symbolic-ref --short HEAD)--$(shell git rev-parse HEAD).sql
+
 
 # https://stackoverflow.com/a/6273809/1826109
 %:
